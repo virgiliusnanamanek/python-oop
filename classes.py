dcee74cb6190c8f95c7d171608a1c4f150d4ed50
virgiliusnanamanek <vickynana034@gmail.com>
2022-11-06 13:01:01 +0700

    create classes.py

---
+++ classes.py
@@ -0,0 +1,15 @@
+class Person:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"Hello, I'm {self.name} and I'm {self.age} years old"
+
+
+first_person = Person("Nana", 20)
+
+print(first_person)
+
+# The __init__() function is called automatically every time the class is being used to create a new object.

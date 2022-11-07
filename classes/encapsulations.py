8a38c7651c51d62e4e6a70c6683d4c8f451bb7c3
virgiliusnanamanek <vickynana034@gmail.com>
2022-11-07 12:13:48 +0700

    encapsulation

---
+++ classes/encapsulations.py
@@ -0,0 +1,22 @@
+class Book:
+    def __init__(self):
+        self.__title = "Harry Potter"
+
+    def get_title(self):
+        print(f"The book's title is: {self.__title}")
+
+    def set_title(self, title):
+        self.__title = title
+
+
+book = Book()
+book.get_title()
+
+book.__title = "The Clementine"
+book.get_title()
+
+book.set_title("The Clementine")
+book.get_title()
+
+
+
